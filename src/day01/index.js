f80be609b26ce041f3374462ae9943bc061c65d9
Adilson <adilsonpjunior@gmail.com>
2022-01-11 15:19:18 -0300

    advent-of-code day01_part2

--- src/day01/index.js
+++ src/day01/index.js
@@ -1,12 +1,31 @@
-import { readInputNumeric } from '../util.js';
+import { readInputNumeric, readInputString } from "../util.js";
 
 const input = readInputNumeric(process.cwd() + "/src/day01/input.txt");
 
-let increased = 0;
+function countIncrease(array) {
+  let increased = 0;
 
-for (let i = 1; i < input.length; i++) {
-  // Inicia no segundo numero
-  increased += input[i] > input[i - 1] ? 1 : 0;
+  for (let i = 1; i < array.length; i++) {
+    // Inicia no segundo numero
+    increased += array[i] > array[i - 1] ? 1 : 0;
+  }
+
+  return increased
 }
 
-console.log(increased);
+console.log("Parte 1");
+
+console.log(countIncrease(input));
+
+console.log("Parte 2");
+
+let measurementsPoints = []
+
+for (let i = 0; i < (input.length - 2); i++) {
+
+  let soma = input[i] + input[i + 1] + input[i + 2]
+  measurementsPoints.push(soma)
+  }
+
+  console.log(countIncrease(measurementsPoints));
+
