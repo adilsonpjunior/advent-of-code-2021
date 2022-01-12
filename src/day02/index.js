33fac92d053913680975e478b5eb6e1327e5fb03
Adilson <adilsonpjunior@gmail.com>
2022-01-12 13:03:26 -0300

    day02_part2

--- src/day02/index.js
+++ src/day02/index.js
@@ -2,17 +2,35 @@ import { readInputNumeric, readInputString } from "../util.js";
 
 const input = readInputString(process.cwd() + "/src/day02/input.txt");
 
-let horizontal = 0
-let depth = 0
+let horizontal = 0;
+let depth = 0;
+let aim = 0;
 
-for(let i = 0; i < input.length; i++){
-    let command = input[i].split(" ");
-    let valueToNumber = Number(command[1])
-
-    horizontal += (command[0] == 'forward' ? valueToNumber : 0 )
-    depth += (command[0] == 'down' ? valueToNumber : 0 )
-    depth -= (command[0] == 'up' ? valueToNumber : 0 )
-
+function calculateDepth(horizontal, aim) {
+  return horizontal * aim;
 }
 
-console.log(horizontal * depth)
+for (let i = 0; i < input.length; i++) {
+  let command = input[i].split(" ");
+  let valueToNumber = Number(command[1]);
+
+//   depth += command[0] == "forward" ? calculateDepth(valueToNumber, aim) : 0;
+//   horizontal += command[0] == "forward" ? valueToNumber : 0;
+//   aim += command[0] == "down" ? valueToNumber : 0;
+//   aim -= command[0] == "up" ? valueToNumber : 0;
+
+  switch (command[0]) {
+    case "forward":
+      depth += calculateDepth(valueToNumber, aim);
+      horizontal += valueToNumber;
+      break;
+    case "down":
+      aim += valueToNumber;
+      break;
+    case "up":
+      aim -= valueToNumber;
+      break;
+  }
+}
+
+console.log(horizontal * depth);
